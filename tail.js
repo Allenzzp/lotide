ef863d3299f1eca04b7659a9be370cfadd5d4f79
Allenzzp <131452abc@gmail.com>
2021-11-23 01:58:38 +0000

    add function tail and modified asserEqaul

---
+++ tail.js
@@ -0,0 +1,33 @@
+const tail = function(aArr) {
+  if (aArr.length > 0) {
+    return aArr.slice(1);
+  } else {
+    return [];
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (typeof actual === "object") {
+    if (actual.length === expected.length) {
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+          console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
+          break;
+        }
+      }
+      console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+// assertEqual(tail([1,2,3,4]), [2,3,4]);
+// assertEqual(tail([]), []);
+// console.log(tail([2]));
