e2f466a537231ecb0d00ebd5ceda5e2538bc84a7
Allenzzp <131452abc@gmail.com>
2021-11-29 22:02:53 +0000

    refactored assertArrayEqual

--- eqArrays.js
+++ eqArrays.js
@@ -21,14 +21,4 @@ const eqArrays = function(arr1, arr2) {
   return false;
 };
 
-// Test cases
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true);
-// assertEqual(eqArrays([], []), true);
-
-// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-
-// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
-
 module.exports = eqArrays;
