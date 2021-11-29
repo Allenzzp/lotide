ec8301ce08634fd4d4daad578c4d50a7da89b766
Allenzzp <131452abc@gmail.com>
2021-11-29 22:06:20 +0000

    refactorred middle

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let report;
-  if (eqArrays(arr1, arr2)) {
-    report = `✅✅✅Assertion Passed: [${arr1}] === [${arr2}]`;
-  } else {
-    report = `❌❌❌Assertion Failed: [${arr1}] !== [${arr2}]`;
-  }
-  console.log(report);
-};
 
 // New code starts
 const middle = function(array) {
@@ -35,7 +13,5 @@ const middle = function(array) {
   }
 };
 
-// Test cases
-// assertArraysEqual(middle([1, 2, 3, 4, 5]) , [3]);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
